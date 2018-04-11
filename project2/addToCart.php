142660e65e73c4ce5ed5b0c46fb3380df783bef9
joshpat9 <jpatrick@csumb.edu>
2018-04-11 04:33:18 +0000

    adding project 2

---
+++ project2/addToCart.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['shoppingCart']))
+    {
+        $arr = array();
+        array_push($arr, $_GET['val']);
+        $_SESSION['shoppingCart'] = $arr;
+    }
+    else {
+        $arr = array();
+        $arr = $_SESSION['shoppingCart'];
+        array_push($arr, $_GET['val']);
+        $_SESSION['shoppingCart'] = $arr;
+    }
+    header('Location: main.php')
+?>
